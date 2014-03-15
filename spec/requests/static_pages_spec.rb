842de979d22a575e86ed48edcc49331fe2045d21
Roi Cohen <roicohen2002@gmail.com>
2014-03-15 14:02:07 +0200

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,31 +8,46 @@ describe "StaticPages" do
   		expect(page).to have_content('Sample App')
   	end
 
-  	it "should have the title 'Home'" do
-  		visit '/static_pages/home'
-  		expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-  	end
+  	it "should have the base title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
   end
 
   describe "Help page" do
   	it "should have the content 'Help'" do
   		visit '/static_pages/help'  		
-  		expect(page).to have_content('Help')
+  		expect(page).to have_content('Sample App')
   	end
-  	it "should have the title 'Help'" do
+  	it "should have the base title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/help'
+      expect(page).not_to have_title('| Help')
     end
   end
 
   describe "About page" do
   	it "should have the content 'About Us'" do
   		visit '/static_pages/about'
-  		expect(page).to have_content('About Us')
+  		expect(page).to have_content('Sample App')
   	end
-  	it "should have the title 'About Us'" do
+  	it "should have the base title" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/about'
+      expect(page).not_to have_title('| About')
     end
   end
 end
